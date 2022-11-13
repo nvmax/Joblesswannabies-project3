9c0cfaa38a99303f93d1d2afb66ca7198e339577
Combatxd <logan.schoerner@gmail.com>
2022-11-13 16:12:20 -0700

    navbar grid layout and light styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div></div>
+    <ApolloProvider client={client}>
+      <Nav />
+    </ApolloProvider>
   )
 }
 
---
+++ client/src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <nav className='nav-grid nav-style'>
+            <div className='gird-col-span-2 nav-title font-Coolvetica'>Garmin</div>
+            <ul className='nav-list-grid nav-list-style font-Poppins'>
+                <li><a>Browse</a></li>
+                <li><a>Browse</a></li>
+                <li><a>Browse</a></li>
+                <li><a>Browse</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
